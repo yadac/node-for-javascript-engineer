280d4f91b91964222ec0f63e90ba05ccdde823ea
Yosuke Adachi <yosuke.adachi@avanade.com>
2019-03-08 19:48:32 +0900

    static file

---
+++ 5/sample.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    var urlPaths = url.parse(req.url);
+    var path = __dirname + '/' + urlPaths.pathname;
+    var stream = fs.createReadStream(path);
+
+    stream.on('data', function(data){
+        res.write(data);
+    });
+    stream.on('end', function(data){
+        res.end();
+    });
+});
+
+server.listen(1234);
+console.log('start server ...');
