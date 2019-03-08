d9e0362883568359d80e49221d89ce630500eaf0
Yosuke Adachi <yosuke.adachi@avanade.com>
2019-03-09 08:27:53 +0900

    stream

--- 5/sample.js
+++ 5/sample.js
@@ -2,17 +2,20 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-var server = http.createServer(function(req, res){
+var server = http.createServer(function (req, res) {
     var urlPaths = url.parse(req.url);
     var path = __dirname + '/' + urlPaths.pathname;
     var stream = fs.createReadStream(path);
+    
+    stream.pipe(res);
 
-    stream.on('data', function(data){
-        res.write(data);
-    });
-    stream.on('end', function(data){
-        res.end();
-    });
+    // stream.on('data', function(data){
+    //     res.write(data);
+    // });
+    // stream.on('end', function(data){
+    //     res.end();
+    // });
+
 });
 
 server.listen(1234);
